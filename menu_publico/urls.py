d39c8b9e1c5c136d6dd503eb4a678a5fe17a45b8
Fer6464 <ariasfernando966@gmail.com>
2026-08-05 10:38:23 -0500

    se creo nuevas views en sistemna_restaurante

---
+++ menu_publico/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = 'menu_publico'
+
+urlpatterns = [
+    path('', views.index, name='menu_index'),
+]
--- sistema_restaurante/urls.py
+++ sistema_restaurante/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from . import views
 
 app_name = 'sistema_restaurante'
 
 urlpatterns = [
-    
+    path('sistema/categorias', views.categorias, name='categorias'),
+    path('sistema/categorias/<int:id>/platos', views.platos_categoria, name='platos_categoria'),
 ]
